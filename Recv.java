12e3d4bb2cb008594f18b790c6f262f1d9ba9d31
mariacasanova25 <mariajoao.albino25@gmail.com>
2023-03-17 18:10:46 +0100

    few changes added

--- Recv.java
+++ Recv.java
@@ -11,6 +11,7 @@ public class Recv {
     private final static String QUEUE_CONSUME = "consume";
     private ArrayList<String> clients = new ArrayList<String>();
     static boolean started = false;
+    static String _id;
 
     static int i = 0;
 
@@ -23,7 +24,7 @@ public class Recv {
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
         channel.queueDeclare(QUEUE_CONSUME, false, false, false, null);
 
-        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        System.out.println("Waiting for PING...");
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
@@ -38,23 +39,29 @@ public class Recv {
             if (messages[0].equals("Start")) {
                 // addRequest(messages[1]);
                 if (!isStarted()) {
-                    start();
-                    System.out.println("Client wants to start");
+                    start(messages[1]);
+                    System.out.println("Client wants to start " + messages[1]);
 
                     String m = "Ping " + messages[1];
                     channel.basicPublish("", QUEUE_CONSUME, null, m.getBytes(StandardCharsets.UTF_8));
                 } else if (isStarted()) {
                     System.out.println("Client needs to wait");
 
-                    String m = "wait";
+                    String m = "wait " + messages[1];
                     channel.basicPublish("", QUEUE_CONSUME, null, m.getBytes(StandardCharsets.UTF_8));
                 }
 
-            } else if (messages[0].equals("Pong")) {
-                System.out.println("PONG");
+            } else if (messages[0].equals("Pong") && messages[1].equals(getId())) {
+                System.out.println(message);
                 if (getI() != 30) {
                     incI();
                     String m = "Ping " + messages[1];
+                    channel.basicPublish("", QUEUE_CONSUME, null, m.getBytes(StandardCharsets.UTF_8));
+                } else {
+                    System.out.println("chegou");
+                    i = 0;
+                    stop();
+                    String m = "end " + messages[1];
                     channel.basicPublish("", QUEUE_CONSUME, null, m.getBytes(StandardCharsets.UTF_8));
                 }
 
@@ -77,8 +84,18 @@ public class Recv {
         i++;
     }
 
-    public static void start() {
+    public static void start(String id) {
         started = true;
+        _id = id;
+    }
+
+    public static void stop() {
+        _id = null;
+        started = false;
+    }
+
+    public static String getId() {
+        return _id;
     }
 
     public static boolean isStarted() {
--- Send.java
+++ Send.java
@@ -3,6 +3,8 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 import java.nio.charset.StandardCharsets;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 public class Send {
 
@@ -16,35 +18,47 @@ public class Send {
             return;
         }
         String id = argv[0];
+        System.out.println("Write ping to start");
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+        String ping = reader.readLine();
+        if (ping.equals("ping")) {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            channel.queueDeclare(QUEUE_CONSUME, false, false, false, null);
 
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        channel.queueDeclare(QUEUE_CONSUME, false, false, false, null);
+            // sending START
+            String message = "Start " + id;
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
 
-        // sending START
-        String message = "Start " + id;
-        channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
-        System.out.println(" [x] Sent '" + message + "'");
+            // Receving
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String m = new String(delivery.getBody(), StandardCharsets.UTF_8);
+                String[] ms = m.split(" ");
+                System.out.println(m);
+                if (ms[0].equals("Ping") && ms[1].equals(id)) {
+                    // System.out.println("o meu id: " + id);
+                    String m2 = "Pong " + id;
+                    channel.basicPublish("", QUEUE_NAME, null, m2.getBytes(StandardCharsets.UTF_8));
+                    // System.out.println("PING");
+                } else if (ms[0].equals("wait") && ms[1].equals(id)) {
 
-        // Receving
-        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String m = new String(delivery.getBody(), StandardCharsets.UTF_8);
-            String[] ms = m.split(" ");
+                    System.out.println("WAIT");
+                    System.exit(0);
 
-            if (ms[0].equals("Ping") && ms[1].equals(id)) {
-                String m2 = "Pong " + id;
-                channel.basicPublish("", QUEUE_NAME, null, m2.getBytes(StandardCharsets.UTF_8));
-                System.out.println("PING");
-            } else if (m.equals("wait")) {
+                } else if (ms[0].equals("end") && ms[1].equals(id)) {
 
-                System.out.println("WAIT");
-            }
-        };
-        channel.basicConsume(QUEUE_CONSUME, true, deliverCallback, consumerTag -> {
-        });
+                    System.out.println("END");
+                    System.exit(0);
+                }
+            };
+            channel.basicConsume(QUEUE_CONSUME, true, deliverCallback, consumerTag -> {
+            });
+        }
+
     }
 
 }
